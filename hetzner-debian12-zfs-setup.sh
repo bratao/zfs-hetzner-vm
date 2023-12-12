0a2a5b81f5893e5fdd660a2dd998c7c95af4cbcd
Piotr Plenzler <61161664+AXYZE9@users.noreply.github.com>
2023-12-12 06:46:31 +0100

    Change defaults

--- hetzner-debian12-zfs-setup.sh
+++ hetzner-debian12-zfs-setup.sh
@@ -40,10 +40,10 @@ v_suitable_disks=()
 c_deb_packages_repo=https://deb.debian.org/debian
 c_deb_security_repo=https://deb.debian.org/debian-security
 
-c_default_zfs_arc_max_mb=250
+c_default_zfs_arc_max_mb=800
 c_default_bpool_tweaks="-o ashift=12 -O compression=lz4"
-c_default_rpool_tweaks="-o ashift=12 -O acltype=posixacl -O compression=zstd-9 -O dnodesize=auto -O relatime=on -O xattr=sa -O normalization=formD"
-c_default_hostname=terem
+c_default_rpool_tweaks="-o ashift=12 -O acltype=posixacl -O compression=zstd-4 -O dnodesize=auto -O relatime=on -O xattr=sa -O normalization=formD"
+c_default_hostname=fivecity
 c_zfs_mount_dir=/mnt
 c_log_dir=$(dirname "$(mktemp)")/zfs-hetzner-vm
 c_install_log=$c_log_dir/install.log
