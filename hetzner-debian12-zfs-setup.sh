e9b1cff6e772531e09e6c10094388b5f2a76d422
Piotr Plenzler <61161664+AXYZE9@users.noreply.github.com>
2024-04-29 14:36:16 +0200

    Update hetzner-debian12-zfs-setup.sh

--- hetzner-debian12-zfs-setup.sh
+++ hetzner-debian12-zfs-setup.sh
@@ -42,7 +42,7 @@ c_deb_security_repo=https://deb.debian.org/debian-security
 
 c_default_zfs_arc_max_mb=800
 c_default_bpool_tweaks="-o ashift=12 -O compression=lz4"
-c_default_rpool_tweaks="-o ashift=12 -O acltype=posixacl -O compression=zstd-2 -O dnodesize=auto -O relatime=on -O xattr=sa -O normalization=formD recordsize=1M"
+c_default_rpool_tweaks="-o ashift=12 -O acltype=posixacl -O compression=zstd-2 -O dnodesize=auto -O relatime=on -O xattr=sa -O normalization=formD -O recordsize=1M"
 c_default_hostname=fivecity
 c_zfs_mount_dir=/mnt
 c_log_dir=$(dirname "$(mktemp)")/zfs-hetzner-vm
