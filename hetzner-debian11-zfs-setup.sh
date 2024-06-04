678eb2649c3ee0f051336c474b36fc923fa017a6
Bruno Cabral <brataodream@gmail.com>
2024-06-04 14:04:30 -0300

    Update hetzner-debian11-zfs-setup.sh

--- hetzner-debian11-zfs-setup.sh
+++ hetzner-debian11-zfs-setup.sh
@@ -554,7 +554,7 @@ zpool create \
   $v_bpool_tweaks -O canmount=off -O devices=off \
   -o cachefile=/etc/zpool.cache \
   -O mountpoint=/boot -R $c_zfs_mount_dir -f \
-  $v_bpool_name $pools_mirror_option "${bpool_disks_partitions[@]}"
+  $v_bpool_name raidz "${bpool_disks_partitions[@]}"
 
 # shellcheck disable=SC2086
 echo -n "$v_passphrase" | zpool create \
@@ -562,7 +562,7 @@ echo -n "$v_passphrase" | zpool create \
   -o cachefile=/etc/zpool.cache \
   "${encryption_options[@]}" \
   -O mountpoint=/ -R $c_zfs_mount_dir -f \
-  $v_rpool_name $pools_mirror_option "${rpool_disks_partitions[@]}"
+  $v_rpool_name raidz "${rpool_disks_partitions[@]}"
 
 zfs create -o canmount=off -o mountpoint=none "$v_rpool_name/ROOT"
 zfs create -o canmount=off -o mountpoint=none "$v_bpool_name/BOOT"
